2dded30e4574bd9fa3188d422b8a6e3ec2ddc0bf
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2024-04-28 15:41:14 +0200

    create source for IO operations

---
+++ madler/myIO.c
@@ -0,0 +1,7 @@
+//
+//  myIO.c
+//
+//  Created by Sebastian Ritter on 28.04.24.
+//
+
+#include "myIO.h"
---
+++ madler/myIO.h
@@ -0,0 +1,12 @@
+//
+//  myIO.h
+//
+//  Created by Sebastian Ritter on 28.04.24.
+//
+
+#ifndef myIO_h
+#define myIO_h
+
+#include <stdio.h>
+
+#endif /* myIO_h */
